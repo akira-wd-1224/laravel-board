424eb557ffc5aad293c8f1a751a371ba26363ffa
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-04 19:17:03 +0900

    仮記事モデルを作成　created_atとuserの追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,18 +17,33 @@ class ArticleController extends Controller
                 'id' => 1,
                 'title' => 'タイトル',
                 'body' => 'ボディ',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 1,
+                    'name' => 'ユーザー名１'
+                ]
             ],
 
             (object)[
                 'id' => 2,
                 'title' => 'タイトル',
                 'body' => 'ボディ',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 2,
+                    'name' => 'ユーザー名２'
+                ]
             ],
 
             (object)[
                 'id' => 3,
                 'title' => 'タイトル',
                 'body' => 'ボディ',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 3,
+                    'name' => 'ユーザー名３'
+                ]
             ],
         ];
         return view('articles.index',['articles'=>$articles]);
