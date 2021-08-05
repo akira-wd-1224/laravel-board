b75db614c749870cfac8d3d2e7a7c0e3fdced5fe
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-05 23:09:56 +0900

    フォームリクエストの作成　バリデーションルールの追加　エラーメッセージのカスタマイズ

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;//ユーザーが更新して良いかどうかを判定
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * キーにパラメーターを、値にバリデーションルールを指定
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' =>'required','max:50',
+            'body' => 'required','max:500',
+        ];
+    }
+
+    /**
+     * バリデーションエラーメッセージに表示される項目名をカスタマイズ
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル',
+            'body' => '本文',
+        ];
+    }
+}
