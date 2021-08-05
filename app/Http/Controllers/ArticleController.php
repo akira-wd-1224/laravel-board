39bf2d4360885de56a59f70164e09fbbb546c77c
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-06 00:36:05 +0900

    editメソッド追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     /**
+     * 記事一覧画面の表示
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
@@ -28,10 +29,10 @@ class ArticleController extends Controller
     }
 
     /**
-     * 記事の登録
-     * @param App\Http\Requests\ArticleRequest
-     * @param App\Article
-     * @return  \Illuminate\Http\RedirectResponse
+     * 記事の登録の処理後記事一覧画面へリダイレクト
+     * @param App\Http\Requests\ArticleRequest $request
+     * @param App\Article $article
+     * @return   Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|Illuminate\Routing\Redirector
      */
     public function store(ArticleRequest $request,Article $article)
     {
@@ -40,5 +41,15 @@ class ArticleController extends Controller
         $article->user_id = $request->user()->id;//リクエストのuserメソッドを使うことでUserクラスのインスタンスにアクセスできる
         $article->save();//articlesテーブルにレコードが新規登録される
         return redirect()->route('articles.index');
+    }
+
+    /**
+     * 記事更新画面の表示
+     * @param App\Article $article
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View|App\Article
+     */
+    public function edit(Article $article)
+    {
+        view('articles.edit',['article'=>$article]);
     }
 }
