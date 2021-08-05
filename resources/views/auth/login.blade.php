c8783c5bbf251cda23180365b89a548e98c3b324
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-05 21:41:56 +0900

    typeをtextに修正　divの位置修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 
-@section('title','ログイン')
+@section('title', 'ログイン')
 
 @section('content')
     <div class="container">
@@ -10,24 +10,24 @@
                 <div class="card mt-3">
                     <div class="card-body text-center">
                         <h2 class="h3 card-title text-center mt-2">ログイン</h2>
-                    </div>
-                    @include('error_card_list')
-                    <div class="card-text">
-                        <form method="POST" action="{{route('login')}}">
-                        @csrf
-                            <div class="md-form">
-                                <label for="email">メールアドレス</label>
-                                <input class="form-control" type="email" id="email" name="email" required value="{{old('email')}}">
+                        @include('error_card_list')
+                        <div class="card-text">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="md-form">
+                                    <label for="email">メールアドレス</label>
+                                    <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+                                </div>
+                                <div class="md-form">
+                                    <label for="password">パスワード</label>
+                                    <input class="form-control" type="password" id="password" name="password" required>
+                                </div>
+                                <input type="hidden" name="remember" id="remember" value="on">
+                                <button class="btn btn-block peach-gradient mt-2 mb-2" type="submit">ログイン</button>
+                            </form>
+                            <div class="mt-0">
+                                <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
                             </div>
-                            <div class="md-form">
-                                <label for="password">パスワード</label>
-                                <input class="form-control" type="password" id="password" name="password" required>
-                            </div>
-                            <input type="hidden" name="remember" id="remember" value="on">
-                            <button class="btn btn-block peach-gradient" type="submit">ログイン</button>
-                        </form>
-                        <div class="mt-0">
-                            <a class="card-text" href="{{route('register')}}">ユーザー登録はこちら</a>
                         </div>
                     </div>
                 </div>
