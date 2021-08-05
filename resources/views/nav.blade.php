3098079e1834dc93844952a62b7dbda712a17005
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-05 15:02:56 +0900

    navバーの追加　各ボタンの追加　ドロップダウンメニューの追加

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand navbar-dark peach-gradient">
+    {{--navbarでバーの表示navbar-expandでレスポンシブ対応navbar-darkは背景を黒くしてテキストを白く表示peach-gradientは背景色変更--}}
+    <a class="navbar-brand" href="/"><i class="fa fa-edit" aria-hidden="true">board</i></a>
+    {{--navbar-brandはブランドを表示--}}
+    {{--navbar-navはハンバーガーメニュー--}}
+    {{--ml-autoはマージンレフトのオート（右端に寄せるため）--}}
+    <ul class="navbar-nav ml-auto">
+        {{--nav-itemは部品の構成nav-linkはリンク部品--}}
+        <li class="nav-item">
+            <a class="nav-link" href="">ユーザー登録</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="">ログイン</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href=""><i class="fas fa-pen mr-1" aria-hidden="true"></i>投稿する</a>
+        </li>
+
+        <!-- Dropdown -->
+        {{--dropdownはdropdown-toggleやdropdown-menu、dropdown-itemを組み合わせて表示させる。aria-＊”...”は読み上げブラウザなどに付加情報を与えるもの--}}
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user-circle"></i>
+            </a>
+            {{--dropdown-menu-rightは下に出すメニューの表示位置を右に合わせる。buttonタグでの画面遷移はonclick="location.href=''"とする--}}
+            <div class="dropdown-menu dropdown-menu-right " aria-labelledby="navbarDropdownMenuLink">
+                <button class="dropdown-item" type="button"
+                        onclick="location.href=''">
+                    マイページ
+                </button>
+                {{--dropdown-dividerは分割線を表示--}}
+                <div class="dropdown-divider"></div>
+                <button form="logout-button" class="dropdown-item" type="submit">
+                    ログアウト
+                </button>
+            </div>
+        </li>
+        {{--レイアウトが崩れるためあえて外にフォームタグを出している--}}
+        <form id="logout-button" method="POST" action="">
+        </form>
+        <!-- Dropdown -->
+
+    </ul>
+</nav>
