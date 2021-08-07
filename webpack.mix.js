7e661a76c735f3de40b4c685e948fa4ff66f347d
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-07 14:03:00 +0900

    コメントの追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .version();
+    .version();//バージョンをつけ最新のjsを読ませるようにキャッシュ対策をしている
