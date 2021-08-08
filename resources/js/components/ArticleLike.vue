983b471e304628ca5dfb7700e02591b3730fdcec
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-08 17:44:07 +0900

    メソッドとプロパティとv-onの追加

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -4,20 +4,32 @@
             type="button"
             class="btn m-0 p-1 shadow-none"
         >
+
             <i class="fas fa-heart mr-1"
                :class="{'red-text' :this.isLikedBy}"
+               @click="clickLike"
             />
         </button>
         {{ countLikes　}}
     </div>
 </template>
 <!--このファイルは単一ファイルコンポーネントとして扱われるのでtemplate、style、scriptを1つのVueファイルでまとめて管理する-->
+<!--:classでv-bindを使用している。プロパティを直接使わない理由は値を変化させるため-->
+<!--テンプレート内でv-on:ディレクティブ（@clickは省略形）を使うことでvueインスタンスのメソッドを呼び出すことができる-->
 <!--外部から参照されつことが前提なのでscriptの部分はexport default で囲むことが前提-->
 <!--propsはプロパティ-->
-<!--dataはオブジェクトを定義している。componentで必要な時にdata()を返すことができる。子のcomponentでは関数で返す必要がある-->
-<!--プロパティを直接使わない理由は値を変化させるため-->
+<!--dataはオブジェクトを定義している。componentで必要な時にdata()を返すことができる。-->
+<!--子のcomponentでは関数で返す必要がある。親componentに影響を与えないため-->
+<!--asyncとawaitは、JavaScriptで非同期処理を簡潔に書くための仕組み-->
+<!--非同期処理を伴う関数定義にasyncをつける。非同期処理を伴う関数実行時にawaitをつける。-->
+<!--axiosはHTTP通信を行うためのJavaScriptのライブラリで
+this.endpointはURIのarticles/{article}/likeに対して、HTTPのPUTメソッドでリクエストする-->
+<!--const responseにはaxiosによるHTTP通信の結果が代入されて、response.dataとすることでレスポンスのボディ部にアクセスできる。
+レスポンスのボディ部にはLaravel側のlikeやunlikeアクションメソッドの戻り値が代入されている。
+なのでresponse.data.countLikesとすることで、いいね数を取得することができる。-->
 <script>
     export default {
+
         props: {
             initialIsLikedBy: {
                 type: Boolean,
@@ -27,12 +39,44 @@
                 type: Number,
                 default: 0,
             },
+            authorized: {
+                type: Boolean,
+                default: false,
+            },
+            endpoint: {
+                type: String,
+            },
         },
+
         data() {
             return {
                 isLikedBy: this.initialIsLikedBy,
                 countLikes: this.initialCountLikes,
             }
+        },
+        methods: {
+            clickLike() {
+                if(!this.authorized) {
+                    alert('いいね機能はログイン中のみ使用できます')
+                    return
+                }
+
+                this.isLikedBy
+                ? this.unlike()
+                : this.like()
+            },
+
+            async like() {
+                const response = await axios.put(this.endpoint)
+                this.isLikedBy = true
+                this.countLikes = response.data.countLikes
+            },
+
+            async unlike() {
+                const response = await axios.delete(this.endpoint)
+                this.isLikedBy = false
+                this.countLikes = response.data.countLikes
+            },
         },
     }
 </script>
