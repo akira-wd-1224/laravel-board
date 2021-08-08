b76fd43697fcf0eff841aba7de73ac6e079d0767
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-08 10:13:33 +0900

    initial-count-likesの追加

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -8,13 +8,14 @@
                :class="{'red-text' :this.isLikedBy}"
             />
         </button>
-        10
+        {{ countLikes　}}
     </div>
 </template>
 <!--このファイルは単一ファイルコンポーネントとして扱われるのでtemplate、style、scriptを1つのVueファイルでまとめて管理する-->
 <!--外部から参照されつことが前提なのでscriptの部分はexport default で囲むことが前提-->
 <!--propsはプロパティ-->
 <!--dataはオブジェクトを定義している。componentで必要な時にdata()を返すことができる。子のcomponentでは関数で返す必要がある-->
+<!--プロパティを直接使わない理由は値を変化させるため-->
 <script>
     export default {
         props: {
@@ -22,10 +23,15 @@
                 type: Boolean,
                 default: false,
             },
+            initialCountLikes: {
+                type: Number,
+                default: 0,
+            },
         },
         data() {
             return {
                 isLikedBy: this.initialIsLikedBy,
+                countLikes: this.initialCountLikes,
             }
         },
     }
