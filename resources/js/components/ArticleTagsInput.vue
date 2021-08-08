269a59d0c604fe80b648735c85c15f743b64d81e
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 00:23:32 +0900

    inputの追加　placeholderの追加　算出プロパティtagsJson()の追加

--- resources/js/components/ArticleTagsInput.vue
+++ resources/js/components/ArticleTagsInput.vue
@@ -1,8 +1,20 @@
 <template>
     <div>
+<!--入力済みのタグ情報はvue上ではtagsに代入されるがHTMLとしてはspanタグ内に存在する。-->
+<!--このままではformタグを使ってタグ情報をPOST送信することができない。-->
+<!--コンポーネント内でtype属性にhiddenである隠しinputタグを作ることでタグに情報を持たせている-->
+        <input
+            type="hidden"
+            name="tags"
+            :value="tagsJson"
+        >
+<!--v-modelで双方向データバイディング-->
+<!--プレースホルダとは、実際の内容を後から挿入するために、とりあえず仮に確保した場所のこと。
+VueTagInputのプレイスホルダー(placeholder)は、デフォルトでは"Add Tag"。"タグを5個まで入力できます"にカスタマイズ-->
         <vue-tags-input
             v-model="tag"
             :tags="tags"
+            placeholder="タグを5個まで入力できます"
             :autocomplete-items="filteredItems"
             @tags-changed="newTags => tags = newTags"
         />
@@ -33,11 +45,16 @@ export default {
             }],
         };
     },
+    //computedは算出プロパティ。結果がキャッシュされる
     computed: {
         filteredItems() {
             return this.autocompleteItems.filter(i => {
                 return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
             });
+        },
+        //データtagsをJSON形式の文字列に変換したものを返す
+        tagsJson() {
+            return JSON.stringify(this.tags)
         },
     },
 };
