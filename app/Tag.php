8a4f038daf63af81c492b66540a4ccd959ac3820
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 17:37:07 +0900

    tagからArticleへのリレーション追加

--- app/Tag.php
+++ app/Tag.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -15,5 +16,9 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return '#' . $this->name;
+    }
+    public function articles() :BelongsToMany
+    {
+        return $this->BelongsToMany('App\Article')->withTimestamps();
     }
 }
