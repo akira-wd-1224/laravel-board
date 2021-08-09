f6c261cabfa6e04572b191cede0be0976f3426c4
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 16:33:38 +0900

    formの修正　allTagNameタグの追加

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -7,9 +7,11 @@
 {{--Bladeからコンポーネントに渡すためにArticleTagsInputコンポーネントには、プロパティInitialTagsを定義して
 InitialTagsにタグ情報の入った変数$tagNamesの値を渡す。$tagNames ?? []としているのは、記事入力フォームのBladeであるform.blade.phpが、
 createアクションメソッド(記事投稿画面)でも使用されているため。そのため、Blade側では、$tagNamesがnullだった時には空の配列を
-ArticleTagsInputコンポーネントに渡せるように考慮--}}
+ArticleTagsInputコンポーネントに渡せるように考慮。$allTagNameも同様。$allTagNamesがnullであることを考慮する必要は無がいいねが
+もしnullが発生した状態を想定して画面は表示できるけれど自動補完がされない状態にする。--}}
     <article-tags-input
         :initial-tags='@json($tagNames ?? [])'
+        :autocomplete-items='@json($allTagName ?? [])'
     >
     </article-tags-input>
 </div>
