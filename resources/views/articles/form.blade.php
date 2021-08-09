1d830cda23f8e72cb4134f8790d5193f8bf499c7
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 15:46:03 +0900

    formの修正　initial-tagsの追加

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -4,7 +4,12 @@
     <input type="text" name="title" class="form-control" required value="{{$article->title ?? old('title') }}">
 </div>
 <div class="form-group">
+{{--Bladeからコンポーネントに渡すためにArticleTagsInputコンポーネントには、プロパティInitialTagsを定義して
+InitialTagsにタグ情報の入った変数$tagNamesの値を渡す。$tagNames ?? []としているのは、記事入力フォームのBladeであるform.blade.phpが、
+createアクションメソッド(記事投稿画面)でも使用されているため。そのため、Blade側では、$tagNamesがnullだった時には空の配列を
+ArticleTagsInputコンポーネントに渡せるように考慮--}}
     <article-tags-input
+        :initial-tags='@json($tagNames ?? [])'
     >
     </article-tags-input>
 </div>
