9d1e6f6d4d1935079ba6303f9e0c4cf945cbcace
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-10 00:27:56 +0900

    リレーションの追加　isFollowedByの追加

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -47,5 +48,24 @@ class User extends Authenticatable
         //カスタマイズしたテキストメールがパスワード再設定メールとして送信される
         //作成した通知クラスである、PasswordResetNotificationクラスのインスタンスを生成し、notifyメソッドに渡す。
         $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
+
+    public function followers(): BelongsToMany
+    {
+        //belongsToManyメソッドの第三引数と第四引数は省略せずに記述。リレーション元/先のテーブル名の単数形_idという規則性がないため。
+        //リレーション元のusersテーブルのidは、中間テーブルのfollowee_idと紐付く
+        //リレーション先のusersテーブルのidは、中間テーブルのfollower_idと紐付く
+        //中間テーブルのカラム名と、リレーション元/先のテーブル名に前述の規則性がない
+        return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')
+            ->withTimestamps();
+    }
+
+    public function isFollowedBy(?User $user): bool
+    {
+        //$this->followersでfollowers経由のuserコレクションを返す
+        return $user
+            ? (bool)$this->followers->where('id', $user->id)->count()
+            : false;
+
     }
 }
