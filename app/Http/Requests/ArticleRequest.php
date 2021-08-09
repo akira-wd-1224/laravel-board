02629abf5b52632331c6560666574e98ec35bd17
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 11:00:24 +0900

    passedValidationの追加

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -45,4 +45,20 @@ class ArticleRequest extends FormRequest
             'tags' => 'タグ',
         ];
     }
+    //passedValidationメソッドは、フォームリクエストのバリデーションが成功した後に自動的に呼ばれるメソッド
+    //tagsテーブルに必要なのはタグ名だけなので整形してからコントローラーに渡すための処理を定義する
+    public function passedValidation()
+    {
+        //json_decode($this->tags)で、JSON形式の文字列であるタグ情報をPHPのjson_decode関数を使って連想配列に変換
+        //Laravelのcollect関数を使ってコレクションに変換
+        $this->tags = collect(json_decode($this->tags))
+            //sliceメソッドを使うと、コレクションの要素が、第一引数に指定したインデックスから第二引数に指定した数だけになる。
+            ->slice(0, 5)
+            //mapメソッドは、コレクションの各要素に対して順に処理を行い、新しいコレクションを作成します。
+            //なのコールバック引数にはmapで使う要素が入る。コールバックの処理内容は$requestTag->textで戻り値はタグ名になる。
+            //mapメソッドによって、コールバックの処理が繰り返されmapメソッドの戻り値はタグ名のコレクションになる。
+            ->map(function ($requestTag) {
+                return $requestTag->text;
+            });
+    }
 }
