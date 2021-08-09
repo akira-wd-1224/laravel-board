8c200aaf86250078304b993e9e9eb5b0217705da
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 11:46:23 +0900

    $fillableの追加

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    //firstOrCreateメソッドやcreateメソッドを使ってモデルを保存する場合、
+    //前提事項としてモデルの$fiilableプロパティにおいて、セットするプロパティ名が記述されている必要がある。
+    protected $fillable = [
+        'name',s
+    ];
 }
