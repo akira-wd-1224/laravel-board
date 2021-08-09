da58afcd1376cd765465f6d0453c8b7b20f7915e
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-09 17:35:40 +0900

    tag一覧表示画面ルートの追加

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,6 @@ Route::prefix('articles')->name('articles.')->group(function(){
     Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
     Route::delete('/{article}/like','ArticleController@unlike')->name('unlike')->middleware('auth');
 });
+//第一引数に{name}とするとTagControllerのアクションメソッドで、引数$nameを受け取ることができる
+Route::get('/tags/{name}','TagController@show')->name('tags.show');
 
