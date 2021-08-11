388801eaf9aaedccdea4622f68872b3906d8ed68
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-11 09:36:18 +0900

    handleProviderCallbackの追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+
 
 class LoginController extends Controller
 {
@@ -54,4 +57,36 @@ class LoginController extends Controller
     {
         return Socialite::driver($provider)->redirect();
     }
+
+
+    public function handleProviderCallback(Request $request, string $provider)
+    {
+        //Socialite::driver($provider)->stateless()->user()により、Laravel\Socialite\Two\Userというクラスのインスタンスを取得
+        //Laravel\Socialite\Two\Userクラスのインスタンスでは、Googleから取得したユーザー情報をプロパティとして持っている。これを変数$providerUser
+        $providerUser = Socialite::driver($provider)->stateless()->user();
+        //$providerUser->getEmail()により、Googleから取得したユーザー情報からメールアドレスを取得
+        //このメールアドレスをwhereメソッドの第二引数に渡し、条件に一致するユーザーモデルをコレクションとして取得
+        //$userには、Googleから取得したメールアドレスと同じメールアドレスを持つユーザーモデルが代入される
+        //同じメールアドレスを持つユーザーが存在しない場合は、$userにはnullが代入される
+        $user = User::where('email', $providerUser->getEmail())->first();
+        //$userがnullでなければ、つまりGoogleから取得したメールアドレスと同じメールアドレスを持つユーザーモデルが存在すれば、
+        //そのユーザーでログイン処理を行なっている。
+        if ($user) {
+            //loginメソッドの第二引数をtrueにるが、こうすることでログアウト操作をしない限り、
+            //ログイン状態が維持されるようになる。remember meトークンが有効になる
+            $this->guard()->login($user, true);
+            //ログイン後の画面(記事一覧画面)へ遷移するようにしてる
+            //AuthenticatesUsersトレイトのloginメソッド内のコードを参考にした
+            return $this->sendLoginResponse($request);
+        }
+        //$providerUser->tokenでは、Googleから発行されたトークンが返る
+        //このトークンがあれば、任意のタイミングでGoogleアカウントのユーザー情報を取得できる
+        return redirect()->route('register.{provider}', [
+            'provider' => $provider,
+            'email' => $providerUser->getEmail(),
+            'token' => $providerUser->token,
+        ]);
+    }
+
 }
+
