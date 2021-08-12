abbaf54ec3f855f6d25445797604cd3c6abedae3
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-13 08:29:13 +0900

    testIsLikedByNullの追加

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -2,21 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ArticleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testIsLikedByNull()
+    {
+        //factory(Article::class)->create()とすることで、ファクトリによって生成されたArticleモデルがデータベースに保存
+        $article = factory(Article::class)->create();
+        //$articleがisLikedByメソッドを使用。引数としてnullを渡し、その戻り値が変数$resultに代入
+        $result = $article->isLikedBy(null);
+        //assertFalseメソッドは、引数がfalseかどうかをテスト
+        $this->assertFalse($result);
     }
 }
