18fc170d178b27d23a54055dcbe913dafa03bf07
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-13 09:04:12 +0900

    testIsLikedByTheUserとtestIsLikedByAnotherの追加

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Article;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,6 +19,31 @@ class ArticleTest extends TestCase
         //$articleがisLikedByメソッドを使用。引数としてnullを渡し、その戻り値が変数$resultに代入
         $result = $article->isLikedBy(null);
         //assertFalseメソッドは、引数がfalseかどうかをテスト
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        //likesテーブルのレコードが新規登録
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        //自分ではない他人が記事に「いいね」をする
+        $article->likes()->attach($another);
+        //$userは、この$articleをいいねしていないユーザー
+        $result = $article->isLikedBy($user);
+
         $this->assertFalse($result);
     }
 }
